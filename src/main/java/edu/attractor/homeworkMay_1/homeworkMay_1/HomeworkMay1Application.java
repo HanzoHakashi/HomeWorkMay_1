2ea92ae1bc59a581cc4abe1d4f87df6abdc7a3ee
Madzhun Khakim <hakimmadgun@gmail.com>
2023-05-03 21:17:48 +0600

    created project in start.spring.io add dependencies

---
+++ src/main/java/edu/attractor/homeworkMay_1/homeworkMay_1/HomeworkMay1Application.java
@@ -0,0 +1,13 @@
+package edu.attractor.homeworkMay_1.homeworkMay_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkMay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkMay1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/attractor/homeworkMay_1/homeworkMay_1/HomeworkMay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.attractor.homeworkMay_1.homeworkMay_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkMay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
