94796080b240500a8451c74d836176947a2c9db1
Madzhun Khakim <hakimmadgun@gmail.com>
2023-05-03 21:52:54 +0600

    created creation table file and created table in my dataBase now i will create repository file and rest files

---
+++ src/main/resources/db/migration/v20230503_1934_init_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE products
+(
+    id bigserial NOT NULL CONSTRAINT products_pkey PRIMARY KEY,
+    name varchar(255),
+    image varchar(255),
+    quantity integer,
+    description varchar(255),
+    price double precision
+)
