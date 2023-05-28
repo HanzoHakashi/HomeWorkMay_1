33dedd8f8a3d7aba5ccb981aae43f62ce744f4da
Madzhun Khakim <hakimmadgun@gmail.com>
2023-05-28 22:47:41 +0600

    Add messages.properties refactor UserInitController

--- src/main/java/edu/attractor/homeworkMay_1/homeworkMay_1/controllers/UserInitController.java
+++ src/main/java/edu/attractor/homeworkMay_1/homeworkMay_1/controllers/UserInitController.java
@@ -6,6 +6,8 @@ import edu.attractor.homeworkMay_1.homeworkMay_1.exeption.UserNotFoundException;
 import edu.attractor.homeworkMay_1.homeworkMay_1.mappers.UserMapper;
 import edu.attractor.homeworkMay_1.homeworkMay_1.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,26 +22,32 @@ import java.util.Random;
 public class UserInitController {
 
     private final UserService userService;
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+    private final MessageSource messageSource;
+
     @GetMapping("/cart")
-    public String testSession(){
+    public String testSession() {
         return "cart";
     }
+
     @GetMapping("/register")
     public String registerGet() {
         return "register";
     }
+
     @GetMapping("/login")
     public String showLoginPage(@RequestParam(value = "logout", required = false) String logout, Model model) {
         if (logout != null) {
-            model.addAttribute("message", "You have been logged out successfully.");
+            model.addAttribute("message", messageSource.getMessage("logout.message", null, LocaleContextHolder.getLocale()));
         }
         return "login";
     }
+
     @GetMapping("/user/forgotPassword")
-    public String getProcessForgotPassword(){
+    public String getProcessForgotPassword() {
         return "findEmailForPassword";
     }
+
     @PostMapping("/user/forgotPassword")
     public String processForgotPassword(HttpServletRequest request, Model model) {
         String email = request.getParameter("email");
@@ -51,20 +59,22 @@ public class UserInitController {
 
         try {
             userService.updateResetPasswordToken(token, email);
-            String resetPasswordToken = "Token: " + token;
-            model.addAttribute("resetPasswordToken", resetPasswordToken);
-            model.addAttribute("message", "Token generated successfully. Please use the token to reset your password.");
-
+            String resetPasswordToken = messageSource.getMessage("reset.token.generated.token" , null, LocaleContextHolder.getLocale());
+            model.addAttribute("resetPasswordToken", resetPasswordToken + token);
+            String resetPasswordMessage = messageSource.getMessage("reset.token.generated.message", null, LocaleContextHolder.getLocale());
+            model.addAttribute("message", resetPasswordMessage);
         } catch (UserNotFoundException ex) {
             model.addAttribute("error", ex.getMessage());
         }
 
         return "forgotPassword";
     }
+
+
     @GetMapping("/reset_password")
     public String showResetPasswordPage(Model model) {
-        model.addAttribute("title", "Reset Password");
-        model.addAttribute("message", "Wait for a new password");
+        model.addAttribute("title", messageSource.getMessage("reset.password.title", null, LocaleContextHolder.getLocale()));
+        model.addAttribute("message", messageSource.getMessage("reset.password.message", null, LocaleContextHolder.getLocale()));
         return "reset_password";
     }
 
@@ -74,31 +84,28 @@ public class UserInitController {
         String password = request.getParameter("password");
 
         User user = userService.getByResetPasswordToken(token);
-        model.addAttribute("title", "Reset Password");
-
+        model.addAttribute("title", messageSource.getMessage("reset.password.title", null, LocaleContextHolder.getLocale()));
 
         if (user == null) {
-            model.addAttribute("message", "Invalid Token");
+            model.addAttribute("message", messageSource.getMessage("reset.token.invalid", null, LocaleContextHolder.getLocale()));
         } else {
             userService.updatePassword(user, password);
-            model.addAttribute("message", "Password changed successfully");
+            model.addAttribute("message", messageSource.getMessage("reset.password.changed", null, LocaleContextHolder.getLocale()));
         }
 
         return "reset_password";
     }
 
-
-
-
     @GetMapping("/profile")
     public String loginSuccess() {
         return "profile";
     }
+
     @PostMapping(path = "/register")
-    public String registerPost(UserDto userDto){
+    public String registerPost(UserDto userDto) {
         userDto.setActive(true);
         userDto.setRole("USER");
         userService.createUser(userDto);
         return "redirect:/login";
-}
+    }
 }
